fe7975e21b1d2eff742d1c62751359153ea659aa
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-05-28 19:01:00 -0300

    Criando o side bar

--- src/components/AsideMenu.tsx
+++ src/components/AsideMenu.tsx
@@ -1,11 +1,17 @@
-export default function AsideMenu() {
+import { FaTimes } from "react-icons/fa";
+
+export default function AsideMenu({ active }) {
+
+  const closeSideBar= () => {
+    active(false)
+  }
   return(
-    <nav className="asideMenu">
-      <a>Inicio</a>
-      <a>Sobre Mim</a>
-      <a>Skills</a>
-      <a>Projetos</a>
-      <a>Contato</a>
-    </nav>
+    <aside
+    className="aside-menu"
+    sideBar={active}>
+      <FaTimes
+      className="fatimes"
+      onClick={closeSideBar}/>
+    </aside>
   )
 }
